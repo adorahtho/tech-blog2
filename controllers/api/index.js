b0c771ebed5a4064b38040db6f61606f52aac318
thoak90codes <adorahtho@gmail.com>
2023-06-15 23:26:32 -0500

    added Blog, index, and User files to models folder

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+onst router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
