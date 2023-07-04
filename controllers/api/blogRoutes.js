1ad2a7d3f7efc5f259e69fd340e6979251be4233
thoak90codes <adorahtho@gmail.com>
2023-07-04 13:43:32 -0500

    changed blogs.js file name in public folder to profile.js so newly created blogs will appear in homepage

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
       ...req.body,
       user_id: req.session.user_id,
     });
-
+console.log(newBlog)
     res.status(200).json(newBlog);
   } catch (err) {
     res.status(400).json(err);
